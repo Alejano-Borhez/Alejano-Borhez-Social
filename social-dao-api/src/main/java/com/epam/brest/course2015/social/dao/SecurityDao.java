69f4d7a1551fc583a893f942a0015299a38fdeda
Alejano-Borhez <alexander.borohov17@gmail.com>
2016-09-22 18:56:31 +0300

    Caching. Moved annotations to interface

--- social-dao-api/src/main/java/com/epam/brest/course2015/social/dao/SecurityDao.java
+++ social-dao-api/src/main/java/com/epam/brest/course2015/social/dao/SecurityDao.java
@@ -1,15 +1,32 @@
 package com.epam.brest.course2015.social.dao;
 
 import com.epam.brest.course2015.social.core.SocialToken;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 
 /**
  * Created by alexander_borohov on 20.7.16.
  */
 public interface SecurityDao {
+
     void addToken(SocialToken token);
+
+    @Caching(
+        evict = {
+            @CacheEvict(cacheNames = {"token"}, key = "#token.token"),
+            @CacheEvict(cacheNames = {"tokenById"}, key = "#token.userId")
+        }
+    )
     void deleteToken(SocialToken token);
+
+    @Cacheable(value = "userIdByToken", key = "#token.userId")
     Integer getUserId(SocialToken token);
+
+    @Cacheable(value = "token", key = "#token")
     SocialToken getToken(String token);
+
+    @Cacheable(value = "tokenById", key = "#userId", unless = "#result == null")
     SocialToken getTokenByUserId(Integer userId);
 
 }
--- social-jpa/src/main/java/com/epam/brest/course2015/social/jpa/SecurityDaoJPA.java
+++ social-jpa/src/main/java/com/epam/brest/course2015/social/jpa/SecurityDaoJPA.java
@@ -2,10 +2,7 @@ package com.epam.brest.course2015.social.jpa;
 
 import com.epam.brest.course2015.social.core.SocialToken;
 import com.epam.brest.course2015.social.dao.SecurityDao;
-import com.epam.brest.course2015.social.test.Logged;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -24,21 +21,16 @@ public class SecurityDaoJPA implements SecurityDao {
     private EntityManager entityManager;
 
     @Override
-    @Logged
     public void addToken(SocialToken token) {
         entityManager.persist(token);
     }
 
     @Override
-    @Logged
-    @CacheEvict(cacheNames = {"token", "tokenById"}, allEntries = true)
     public void deleteToken(SocialToken token) {
         entityManager.remove(token);
     }
 
     @Override
-    @Logged
-    @Cacheable("userIdByToken")
     public Integer getUserId(SocialToken token) {
         SocialToken gotToken = entityManager.find(SocialToken.class, token.getToken());
         if (gotToken != null) {
@@ -49,15 +41,11 @@ public class SecurityDaoJPA implements SecurityDao {
     }
 
     @Override
-    @Logged
-    @Cacheable("token")
     public SocialToken getToken(String token) {
         return entityManager.find(SocialToken.class, token);
     }
 
     @Override
-    @Logged
-    @Cacheable("tokenById")
     public SocialToken getTokenByUserId(Integer userId) {
         List<SocialToken> tokenList = entityManager
                 .createQuery(getTokenById, SocialToken.class)
